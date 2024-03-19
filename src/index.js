f749c78e40ec5468210efbf7771daf162fdb6bc0
Roxy <155686159+RoxyKeatings@users.noreply.github.com>
2024-03-19 09:44:14 -0400

    added JavaScript for dates&timezones + modified HTML

--- src/index.js
+++ src/index.js
@@ -1,1 +1,32 @@
+function updateTime() {
+  let torontoElement = document.querySelector("#toronto");
+  let torontoDateElement = torontoElement.querySelector(".date");
+  let torontoTimeElement = torontoElement.querySelector(".time");
+  let torontoTime = moment().tz("America/Toronto");
 
+  torontoDateElement.innerHTML = torontoTime.format("MMMM Do YYYY");
+  torontoTimeElement.innerHTML = torontoTime.format(
+    "h:mm:ss [<small>] A [</small>]"
+  );
+
+  let washingtonElement = document.querySelector("#washington");
+  let washingtonDateElement = washingtonElement.querySelector(".date");
+  let washingtonTimeElement = washingtonElement.querySelector(".time");
+  let washingtonTime = moment().tz("America/Vancouver");
+
+  washingtonDateElement.innerHTML = washingtonTime.format("MMMM Do YYYY");
+  washingtonTimeElement.innerHTML = washingtonTime.format(
+    "h:mm:ss [<small>] A [</small>]"
+  );
+  let tokyoElement = document.querySelector("#tokyo");
+  let tokyoDateElement = tokyoElement.querySelector(".date");
+  let tokyoTimeElement = tokyoElement.querySelector(".time");
+  let tokyoTime = moment().tz("Asia/Tokyo");
+
+  tokyoDateElement.innerHTML = tokyoTime.format("MMMM Do YYYY");
+  tokyoTimeElement.innerHTML = tokyoTime.format(
+    "h:mm:ss [<small>] A [</small>]"
+  );
+}
+updateTime();
+setInterval(updateTime, 1000);
